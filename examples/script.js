037e2f853070fb8d701668583fad179a482ab8c5
Filip Nikolas Parák <filip.parak@yeself.com>
2021-03-05 08:46:29 +0100

    First commit

---
+++ examples/script.js
@@ -0,0 +1,9 @@
+(function (i, s, o, g, r, a, m) {
+  i['YesElfApi'] = r; i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.id = 'yesElf'; a.src = g; m.parentNode.insertBefore(a, m.nextSibling)
+})
+  (window, document, 'script', 'https://yeself-cloud-acc-app.brux.space:443/sdk/7aeb2b05-545a-4558-9a4c-7652e88b143e/app.js', 'yesElf');
+yesElf('id', '7aeb2b05-545a-4558-9a4c-7652e88b143e');
+yesElf('popups', true);
